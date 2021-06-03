fda4d8ff905b455a04a7efb63070663056aafdec
hangtrongsang <85291477+hangtrongsang@users.noreply.github.com>
2021-06-03 18:51:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Cậu à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'Thích chứ hihi :3',
+    btnNo: 'Không, mơ hả đuyz >:((',
+    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích tớ nè <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'tại vì tớ yêu cậu rất nhiều, tui yêu cậu từ đây tới mặt trăng và quay lại ă <3',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Tối nay rãnh ib tớ luôn nha cậu hihi <3 .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/profile.php?id=100027544717156' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
